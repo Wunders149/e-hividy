80a9398e36e138b0071ed895648e18df8711c77e
wunders149 <razafimahefaphilibert7@gmail.com>
2026-02-21 14:12:42 +0300

    Good

---
+++ test-db.js
@@ -0,0 +1,5 @@
+const db = require('./config/db');
+console.log('Type of db:', typeof db);
+console.log('Is db.getConnection a function?', typeof db.getConnection === 'function');
+console.log('Keys of db:', Object.keys(db));
+process.exit(0);
